d05b2e1ba6676f57031162d63907e4009a59453c
Bruce Chung <brucekchung@gmail.com>
2018-02-25 10:50:18 -0700

    Add graph for setiment vs time graph

--- src/components/Bitcoin/Bitcoin.js
+++ src/components/Bitcoin/Bitcoin.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { getPrices } from '../../api/apiCall'
+import { getPrices, allTwitter } from '../../api/apiCall'
 import { cleanCryptoData, weekCryptoData } from '../../cleaner'
 import { Chart } from 'react-google-charts'
 import { LineChart } from 'react-chartkick'
+import { compareData, compareTwitterBit, compareTwitWeekBit } from '../../normalize'
 window.Chart = require('chart.js')
 
 export class Bitcoin extends Component {
@@ -12,23 +13,61 @@ export class Bitcoin extends Component {
   }
 
   async componentDidMount () {
-    //also need:
+    //also need: PROMISE.ALL
     //twitter mentions, twitter x price
-
+    const twitterData = await allTwitter()
     const bitcoinPrice = await getPrices('BTC')
+
     const cleanBit = cleanCryptoData(bitcoinPrice)
     const weekBit = weekCryptoData(bitcoinPrice)
-    
+    const weekBitData = weekCryptoData(bitcoinPrice)
+    const twitNormalized = compareTwitterBit(twitterData)
+    const weekTwitNormalized = compareTwitWeekBit(twitterData)
+    const compareBit = compareData(cleanBit)
+    const weekBitComp = compareData(weekBitData)
+
     this.setState({
       current: cleanBit,
-      week: weekBit
+      week: weekBit,
+      dayTwit: twitNormalized,
+      weekTwit: weekTwitNormalized,
+      compareBit: compareBit,
+      compareBitWeek: weekBitComp
     })
   }
 
   render () {
+    const compareDay = [
+      { "name": "price", 
+        "data": this.state.compareBit },
+      { "name": "twitter sentiment",
+        "data": this.state.dayTwit }
+    ]
+
+    const compareWeek = [
+      { "name": "price", 
+        "data": this.state.compareBitWeek },
+      { "name": "twitter sentiment",
+        "data": this.state.weekTwit }
+    ]
+
     return (
       <div className="Bitcoin">
         Bitcoin
+        <LineChart data={compareDay}
+             title='Bitcoin sentiment on Twitter - 1 day (last 24 hrs)'
+             xtitle='Time'
+             ytitle='Positivity (twitter analysis)'
+             min={null}
+             max={null}
+             library={{height: "500px"}} />
+        <LineChart data={compareWeek}
+             title='Bitcoin sentiment on Twitter - 1 week (last 168 hrs)'
+             xtitle='Time'
+             ytitle='Positivity (twitter analysis)'
+             min={null}
+             max={null}
+             library={{height: "500px"}} />
       {
         this.state.week &&
         <LineChart data={this.state.week}
